8b92cdac1e8e76e9f2cf82a71ad7abcc0597a803
Brian <brian@convolve.com>
2015-07-21 23:18:27 -0400

    autohooks

--- codegenius/routes/index.js
+++ codegenius/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var bcrypt = require('bcrypt');
+var crypto = require('crypto');
+var exec = require('child_process').exec;
+creds = require('../credentials');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/git-update', function(req, res, next) {
+  var hmac = 'sha1=' + crypto.createHmac('sha1', creds.git_secret).update(req.rawBody).digest('hex');
+  if(req.headers['x-hub-signature'] == hmac) {
+    exec('git pull', {}, function(error, stdout, stderr) {
+      if(error) {
+        res.json(error);
+        error.handled = true;
+        next(error);
+      } else {
+        res.json({stdout: stdout, stderr: stderr});
+      }
+    });
+  } else {
+    res.json({stderr: 'Invalid secret.'});
+  }
+});
 module.exports = router;
