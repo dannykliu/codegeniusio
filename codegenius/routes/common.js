e6dc2878f336be563a769cfccdcc9904d57e4d0d
Brian <brian@convolve.com>
2015-07-22 22:52:46 -0400

    real mongo stuff

---
+++ codegenius/routes/common.js
@@ -0,0 +1,29 @@
+express = require('express');
+creds   = require('../credentials');
+
+// Sets up mongoose
+mongoose = require('mongoose');
+Schema = mongoose.Schema;
+mongoose.connect(creds.mongo_url);
+
+userSchema = new Schema({
+  email: String,
+  hash: String,
+  fname: String,
+  lname: String,
+  problem: String,
+  language: String
+});
+
+renderGeneric = function(page, vars, res) {
+  express().render(page + '.ejs', vars, function(err, html) {
+    if(err) {
+      console.log(err);
+    } else {
+      vars.content = html;
+      res.render('common', vars);
+    }
+  });
+};
+
+Users = mongoose.model('users', userSchema);
