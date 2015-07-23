0d689ab4ba17acf2aab58d14ec536ea692380d94
Brian <brian@convolve.com>
2015-07-23 14:05:01 -0400

    user register

--- codegenius/routes/index.js
+++ codegenius/routes/index.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
+var bcrypt = require('bcrypt');
 var exec = require('child_process').exec;
+
 creds = require('../credentials');
 require('./common');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
 
+router.get('/user', function(req, res) {
+  res.render('userRegister', {});
+});
+
+router.post('/user', function(req, res) {
+  bcrypt.hash(req.body.password.toString(), 10, function(err, hashpass) {
+    Users.create({
+      email: req.body.email,
+      hash: hashpass,
+      fname: req.body.fname,
+      lname: req.body.lname,
+      problem: req.body.probelm,
+      language: req.body.language
+    }, function(err, user){
+      console.log(user);
+      res.render('index', {});
+    });
+  });
+});
+  
 router.post('/git-update', function(req, res, next) {
   var hmac = 'sha1=' + crypto.createHmac('sha1', creds.git_secret).update(req.rawBody).digest('hex');
   if(req.headers['x-hub-signature'] == hmac) {
