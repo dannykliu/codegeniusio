8cdbfd141a4e6cd16c7a166c8430952d946d8dda
Brian <brian@convolve.com>
2015-07-30 00:14:26 -0400

    initial cards

--- codegenius/routes/experts.js
+++ codegenius/routes/experts.js
@@ -14,7 +14,10 @@ var render = function(page, options, res) {
 router.use('/', forceLogIn);
 /* GET users listing. */
 router.get('/', function(req, res) {
-  render('home', {alert: req.query.alert}, res);
+  Tickets.find({}, function(err, tickets) {
+    console.log(tickets);
+    render('home', {tickets: tickets}, res);
+  });
 });
 
 module.exports = router;
