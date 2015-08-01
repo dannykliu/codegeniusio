be6fff1f481cc0aacc5dcf096ec64902949273b0
Brian <brian@convolve.com>
2015-08-01 17:24:39 -0400

    live code editing

--- codegenius/public/js/live.js
+++ codegenius/public/js/live.js
@@ -4,6 +4,7 @@ editor.setFontSize(18);
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
 
+// Live editing
 var socket = io.connect();
 socket.on('editorUpdate', function (data) {
     editor.setByAPI = true;
@@ -20,6 +21,7 @@ editor.on('change', function() {
     }
 });
 
+// Camera stuff
 var webrtc = new SimpleWebRTC({
 	localVideoEl: 'localVideo',
 	remoteVideosEl: 'remoteVideo',
@@ -28,4 +30,49 @@ var webrtc = new SimpleWebRTC({
 
 webrtc.on('readyToCall', function () {
 	webrtc.joinRoom('Test');
+});
+
+//// Screen Sharing
+var button = document.getElementById('screenShareButton'),
+    setButton = function (bool) {
+        button.innerText = bool ? 'share screen' : 'stop sharing';
+    };
+if (!webrtc.capabilities.screenSharing) {
+    button.disabled = 'disabled';
+}
+webrtc.on('localScreenRemoved', function () {
+    setButton(true);
+});
+
+setButton(true);
+
+button.click(function () {
+    if (webrtc.getLocalScreen()) {
+        webrtc.stopScreenShare();
+        setButton(true);
+    } else {
+        webrtc.shareScreen(function (err) {
+            if (err) {
+                setButton(true);
+            } else {
+                setButton(false);
+            }
+        });
+
+    }
+});
+
+// local screen obtained
+webrtc.on('localScreenAdded', function (video) {
+    video.onclick = function () {
+        video.style.width = video.videoWidth + 'px';
+        video.style.height = video.videoHeight + 'px';
+    };
+    document.getElementById('localScreenContainer').appendChild(video);
+    $('#localScreenContainer').show();
+});
+// local screen removed
+webrtc.on('localScreenRemoved', function (video) {
+    document.getElementById('localScreenContainer').removeChild(video);
+    $('#localScreenContainer').hide();
 });
