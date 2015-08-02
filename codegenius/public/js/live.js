1158755fdac34ef90b16593658854a443c78ce01
Brian <brian@convolve.com>
2015-08-02 14:42:50 -0400

    editing styles

--- codegenius/public/js/live.js
+++ codegenius/public/js/live.js
@@ -32,47 +32,73 @@ webrtc.on('readyToCall', function () {
 	webrtc.joinRoom('Test');
 });
 
-//// Screen Sharing
-var button = document.getElementById('screenShareButton'),
-    setButton = function (bool) {
-        button.innerText = bool ? 'share screen' : 'stop sharing';
-    };
-if (!webrtc.capabilities.screenSharing) {
-    button.disabled = 'disabled';
-}
-webrtc.on('localScreenRemoved', function () {
-    setButton(true);
-});
+webrtc.on('videoAdded', function (video, peer) {
+    console.log('video added', peer);
+    var remotes = document.getElementById('remotes');
+    if (remotes) {
+        var container = document.createElement('div');
+        container.className = 'videoContainer';
+        container.id = 'container_' + webrtc.getDomId(peer);
+        container.style = 'height: 100px;'
+        container.appendChild(video);
 
-setButton(true);
+        // suppress contextmenu
+        video.oncontextmenu = function () { return false; };
 
-button.click(function () {
-    if (webrtc.getLocalScreen()) {
-        webrtc.stopScreenShare();
-        setButton(true);
-    } else {
-        webrtc.shareScreen(function (err) {
-            if (err) {
-                setButton(true);
-            } else {
-                setButton(false);
-            }
-        });
-
+        remotes.appendChild(container);
     }
 });
 
-// local screen obtained
-webrtc.on('localScreenAdded', function (video) {
-    video.onclick = function () {
-        video.style.width = video.videoWidth + 'px';
-        video.style.height = video.videoHeight + 'px';
-    };
-    document.getElementById('localScreenContainer').appendChild(video);
-    $('#localScreenContainer').show();
+webrtc.on('videoRemoved', function (video, peer) {
+    console.log('video removed ', peer);
+    var remotes = document.getElementById('remotes');
+    var el = document.getElementById(peer ? 'container_' + webrtc.getDomId(peer) : 'localScreenContainer');
+    if (remotes && el) {
+        remotes.removeChild(el);
+    }
 });
-// local screen removed
-webrtc.on('localScreenRemoved', function (video) {
-    document.getElementById('localScreenContainer').removeChild(video);
-    $('#localScreenContainer').hide();
-});
+
+//// Screen Sharing
+//var button = document.getElementById('screenShareButton'),
+//    setButton = function (bool) {
+//        button.innerText = bool ? 'share screen' : 'stop sharing';
+//    };
+//if (!webrtc.capabilities.screenSharing) {
+//    button.disabled = 'disabled';
+//}
+//webrtc.on('localScreenRemoved', function () {
+//    setButton(true);
+//});
+//
+//setButton(true);
+//
+//button.click(function () {
+//    if (webrtc.getLocalScreen()) {
+//        webrtc.stopScreenShare();
+//        setButton(true);
+//    } else {
+//        webrtc.shareScreen(function (err) {
+//            if (err) {
+//                setButton(true);
+//            } else {
+//                setButton(false);
+//            }
+//        });
+//
+//    }
+//});
+//
+//// local screen obtained
+//webrtc.on('localScreenAdded', function (video) {
+//    video.onclick = function () {
+//        video.style.width = video.videoWidth + 'px';
+//        video.style.height = video.videoHeight + 'px';
+//    };
+//    document.getElementById('localScreenContainer').appendChild(video);
+//    $('#localScreenContainer').show();
+//});
+//// local screen removed
+//webrtc.on('localScreenRemoved', function (video) {
+//    document.getElementById('localScreenContainer').removeChild(video);
+//    $('#localScreenContainer').hide();
+//});
